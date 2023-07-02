f7f48c62ef3eeef150f383f39d281fdc93f1272c
namy2 <namy2@mcmaster.ca>
2023-07-02 20:01:06 +0900

    finished prelim setupDatabase js file to create data tables if not present in db server

--- setupDatabase.js
+++ setupDatabase.js
@@ -2,9 +2,71 @@ const { Client } = require("pg");
 const { DB } = require("./config");
 
 (async () => {
-  const usersTableStmt = `
+
+  const usersTableStatement = `
     CREATE TABLE IF NOT EXISTS users (
-      id              INT               PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL
+      id              INT               PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+      email           VARCHAR(50),
+      passwordHash    TEXT,
+      firstName       VARCHAR(50),
+      lastName        VARCHAR(50)
+    );
+  `
+  
+  const productsTableStatement = `
+    CREATE TABLE IF NOT EXISTS products (
+      id              INT               PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+      name            VARCHAR(50)       NOT NULL,
+      price           DECIMAL(10,2)     NOT NULL,
+      description     VARCHAR(200)       NOT NULL
+    );
+  `
+
+  const ordersTableStatement = `
+    CREATE TABLE IF NOT EXISTS orders (
+      id              INT               PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+      total           INT               NOT NULL,
+      status          VARCHAR(50)       NOT NULL,
+      created         DATE              NOT NULL,
+      modified        DATE              NOT NULL,
+      userId          INT               NOT NULL,
+      FOREIGN KEY (userId) REFERENCES users(id)
+    );
+  `
+
+  const orderItemsTableStatement = `
+    CREATE TABLE IF NOT EXISTS orderItems (
+      id              INT               PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+      qty             INT               NOT NULL,
+      created         DATE              NOT NULL,
+      price           DECIMAL(10,2)     NOT NULL,
+      name            VARCHAR(50)       NOT NULL,
+      description     VARCHAR(200)      NOT NULL,
+      orderId         INT               NOT NULL,
+      productId       INT               NOT NULL,
+      FOREIGN KEY (orderId) REFERENCES orders(id),
+      FOREIGN KEY (productId) REFERENCES products(id)
+    );
+  `
+
+  const cartsTableStatement = `
+    CREATE TABLE IF NOT EXISTS carts (
+      id              INT               PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,       
+      created         DATE              NOT NULL,
+      modified        DATE              NOT NULL,
+      userId          INT               NOT NULL,
+      FOREIGN KEY (userId) REFERENCES users(id)
+    );
+  `
+
+  const cartItemsTableStatement = `
+    CREATE TABLE IF NOT EXISTS cartItems (
+      id              INT               PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+      qty             INT               NOT NULL,
+      productId       INT               NOT NULL,
+      cartId          INT               NOT NULL,
+      FOREIGN KEY (cartId) REFERENCES carts(id),
+      FOREIGN KEY (productId) REFERENCES products(id)
     );
   `
 
@@ -19,8 +81,13 @@ const { DB } = require("./config");
 
     await db.connect();
 
-    // create tables on db 
-    await db.query(usersTableStmt); // users table
+    // create tables on db
+    await db.query(usersTableStatement);
+    await db.query(productsTableStatement);
+    await db.query(ordersTableStatement); 
+    await db.query(orderItemsTableStatement);
+    await db.query(cartsTableStatement);
+    await db.query(cartItemsTableStatement);
 
     await db.end();
 
