2989eab4ae948790c099b7d15109957dc82c9c12
namy2 <namy2@mcmaster.ca>
2023-07-07 22:23:37 +0900

    init order route endpoint services

---
+++ server/controllers/order.js
@@ -0,0 +1,84 @@
+const OrderService = require("../services/orderServices");
+const OrderServiceInstance = new OrderService();
+
+module.exports = {
+
+    // create: async (req, res, next) => {
+    //     try {
+
+    //         const { userid } = req.body;
+    //         const response = await CartServiceInstance.create(userid);
+
+    //         res.status(200).send(response);
+
+    //     } catch(err) {
+    //         next(err);
+    //     }
+    // },
+
+    // getCart: async (req, res, next) => {
+    //     try {
+            
+    //         const { userid } = req.body;
+    //         const response = await CartServiceInstance.getCart(userid);
+
+    //         res.status(200).send(response);
+
+    //     } catch(err) {
+    //         next(err);
+    //     }
+    // },
+
+    // addItem: async (req, res, next) => {
+    //     try {
+            
+    //         const { userid, ...data } = req.body;
+    //         const response = await CartServiceInstance.addItem(userid, data);
+
+    //         res.status(200).send(response);
+
+    //     } catch(err) {
+    //         next(err);
+    //     }
+    // },
+
+    // updateItem: async (req, res, next) => {
+    //     try {
+
+    //         const { cartitemid } = req.params;
+    //         const data = req.body;
+    //         const response = await CartServiceInstance.updateItem(cartitemid, data);
+
+    //         res.status(200).send(response);
+
+    //     } catch(err) {
+    //         next(err);
+    //     }
+    // },
+
+    // removeItem: async (req, res, next) => {
+    //     try {
+
+    //         const { cartitemid } = req.params;
+    //         const response = await CartServiceInstance.removeItem(cartitemid);
+
+    //         res.status(200).send(response);
+
+    //     } catch(err) {
+    //         next(err);
+    //     }
+    // },
+
+    // checkout: async(req, res, next) => {
+    //     try {
+
+    //         const { userid, cartid, paymentinfo } = req.body;
+    //         const response = await CartServiceInstance.checkout(cartid, userid, paymentinfo);
+
+    //         res.status(200).send(response);
+
+    //     } catch(err) {
+    //         next(err);
+    //     }
+    // }
+}
