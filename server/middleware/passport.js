b3146cf8c3bb3e3c59cf0483d280d50096041a69
namy2 <namy2@mcmaster.ca>
2023-07-09 02:03:52 +0900

    cleaned up passport code

--- server/middleware/passport.js
+++ server/middleware/passport.js
@@ -6,17 +6,15 @@ const AuthenticationServiceInstance = new AuthenticationService();
 
 module.exports = (app) => {
 
-    // init passport
     app.use(passport.initialize());
     app.use(passport.session());
 
-    // method to serialize data to store in cookie
+    // serialize data to store in cookie
     passport.serializeUser((user, done) => { done(null, user.id) });
 
-    // method to deserialize data stored in cookie and attach to req.user
+    // deserialize data stored in cookie and attach to req.user
     passport.deserializeUser((id, done) => { done(null, { id }) });
 
-    // local strategy to be use for local login
     passport.use(new LocalStrategy(
         async (username, password, done) => {
             try {
