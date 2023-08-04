6ef10e734c0e9e32a7f718170401a231e0c6d534
Yun Hye Nam <yunnam0924@gmail.com>
2023-08-04 04:32:06 -0400

    sign in validation + product page css

--- frontend/src/components/pages/Register.jsx
+++ frontend/src/components/pages/Register.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios'
 import RegisterForm from "../subcomponents/RegisterForm"
-import checkIfLoggedIn from "../../checkAuth"
+import "./Register.css"
 
 export function Register({userId}) {
 
