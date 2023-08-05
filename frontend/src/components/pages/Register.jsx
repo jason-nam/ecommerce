5ab011ecac15c752f05aeefcfd3521c91cf4070a
Yun Hye Nam <yunnam0924@gmail.com>
2023-08-05 01:15:53 -0400

    css correction + auth

--- frontend/src/components/pages/Register.jsx
+++ frontend/src/components/pages/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import RegisterForm from "../subcomponents/RegisterForm"
-import "./Register.css"
+import "./Login.css"
 
 export function Register({userId}) {
 
@@ -30,10 +30,11 @@ export function Register({userId}) {
 
     if (userId === -1) 
         return (
-            <div>
+            <>
+                <h1 className = "auth-title">Register</h1>
                 <RegisterForm setRegistered={setRegistered} setUserExists={setUserExists}/>
                 {userExists ? <div className="warning">User Already Exists</div> : null}
-            </div>
+            </>
         )
     else if (userId != null)
         return <div>Already logged in. Soon to be redirected</div>
