fc738aa80349c31b712100986699abd6f9d7058a
namy2 <namy2@mcmaster.ca>
2023-08-15 18:35:08 +0900

    subcategory services and models complete in backend

--- backend/controllers/subcategory.js
+++ backend/controllers/subcategory.js
@@ -5,7 +5,11 @@ module.exports = {
 
     getSubcategoriesByCategoryId: async (req, res, next) => {
         try {
-            //TODO
+            
+            const { categoryid } = req.params;
+            const response = await CategoryServiceInstance.getSubcategoriesByCategoryId(categoryid);
+
+            res.status(200).send(response);
 
         } catch(err) {
             res.status(404).send({"message": "404"});
@@ -15,40 +19,58 @@ module.exports = {
 
     getSubcategoryById: async (req, res, next) => {
         try {
-            //TODO
+            
+            const { subcategoryid } = req.params;
+            const response = await CategoryServiceInstance.getSubcategoryById(subcategoryid);
+
+            res.status(200).send(response);
 
         } catch(err) {
-            res.status(404).send({"message": "Product does not exist"});
+            res.status(404).send({"message":  "404"});
             next(err);
         }
     },
 
     createSubcategory: async (req, res, next) => {
         try {
-            //TODO
+            
+            const { categoryid } = req.params;
+            const data = req.body;
+            const response = await CategoryServiceInstance.createSubcategory({ categoryid, ...data });
+
+            res.status(200).send(response);
 
         } catch(err) {
-            res.status(404).send({"message": "Product does not exist"});
+            res.status(404).send({"message":  "404"});
             next(err);
         }
     },
 
     updateSubcategory: async (req, res, next) => {
         try {
-            //TODO
+            
+            const { subcategoryid } = req.params;
+            const data = req.body;
+            const response = await CategoryServiceInstance.updateSubcategory({ subcategoryid, ...data });
+
+            res.status(200).send(response);
 
         } catch(err) {
-            res.status(404).send({"message": "Product does not exist"});
+            res.status(404).send({"message":  "404"});
             next(err);
         }
     },
 
     deleteSubcategory: async (req, res, next) => {
         try {
-            //TODO
+            
+            const { subcategoryid } = req.params;
+            const response = await CategoryServiceInstance.deleteSubcategory(subcategoryid);
+
+            res.status(200).send(response);
 
         } catch(err) {
-            res.status(404).send({"message": "Product does not exist"});
+            res.status(404).send({"message":  "404"});
             next(err);
         }
     }
