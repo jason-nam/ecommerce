fdda1061fb5fe5ed74afbc3e4ec77a381c7120d0
namy2 <namy2@mcmaster.ca>
2023-08-22 16:22:41 +0900

    set userid in orders table to foreign key, but allow null

--- frontend/src/components/pages/Checkout.jsx
+++ frontend/src/components/pages/Checkout.jsx
@@ -249,27 +249,27 @@ export function Checkout({userId, cart, setCart, subtotal, stateCH, orderToast, 
                         <div className="title"> Payment </div>
                     </div>
                     <div className="radio-buttons-group">
-                    <div className="radio-button-container">
-                        <input type="radio" 
-                            name="payment-type" 
-                            id="debit-credit-radio" 
-                            className="radio-button" 
-                            value="credit" 
-                            defaultChecked={creditChecked} 
-                            onChange={e => setCreditChecked(e.target.value)} 
-                        />
-                        <label htmlFor="debit-credit-radio" className="radio-button-label">Credit</label>
-                    </div>
-                    <div className="radio-button-container">
-                        <input type="radio" 
-                            name="payment-type" 
-                            id="paypal-radio" 
-                            className="radio-button" 
-                            value="paypal" 
-                            onChange={e => setCreditChecked(e.target.value)} 
-                        />
-                        <label htmlFor="paypal-radio" className="radio-button-label">Paypal</label>
-                    </div>
+                        <div className="radio-button-container">
+                            <input type="radio" 
+                                name="payment-type" 
+                                id="debit-credit-radio" 
+                                className="radio-button" 
+                                value="credit" 
+                                defaultChecked={creditChecked} 
+                                onChange={e => setCreditChecked(e.target.value)} 
+                            />
+                            <label htmlFor="debit-credit-radio" className="radio-button-label">Credit</label>
+                        </div>
+                        <div className="radio-button-container">
+                            <input type="radio" 
+                                name="payment-type" 
+                                id="paypal-radio" 
+                                className="radio-button" 
+                                value="paypal" 
+                                onChange={e => setCreditChecked(e.target.value)} 
+                            />
+                            <label htmlFor="paypal-radio" className="radio-button-label">Paypal</label>
+                        </div>
                     </div>
                     <div className="debit-credit" style={creditChecked!=="credit"? {display: 'none'} : {}}>
                         <div className={`cinfo ${errors.cardnum? 'error' : '' }`}>
